ee5832d7d887cea65920aee7bfeb03a71a4eb45e
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-07-03 21:08:47 +0000

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (
-    !(set instanceof Set)
-    || typeof startString !== 'string'
-    || startString.length === 0
-  ) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
 
